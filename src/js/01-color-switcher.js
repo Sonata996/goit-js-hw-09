35ef8e7b420a5b86b489370d49e6791157b3a12d
Sonata996 <129296155+Sonata996@users.noreply.github.com>
2023-08-15 15:27:50 +0300

    task number one

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+const body = document.querySelector('body')
+const btnStart = document.querySelector('[data-start]')
+const btnStop = document.querySelector('[data-stop]')
 
+btnStart.addEventListener('click', onClickStartRandomColor)
+btnStop.addEventListener('click', onClickStopRandomColor)
+
+
+function onClickStartRandomColor() {
+    body.style.backgroundColor = getRandomHexColor()
+    document.querySelector('[data-start]').disabled = true;
+    timeInterval = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor()
+    }, 1000)
+    
+}
+function onClickStopRandomColor() {
+    clearInterval(timeInterval)
+    document.querySelector('[data-start]').disabled = false;
+}
